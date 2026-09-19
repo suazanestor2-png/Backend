bb497d2ee2edb49ced25f52e5eee1ad5789128f9
SuazaFai <suazanestor2@gmail.com>
2026-09-18 20:42:05 -0500

    Correciones y agregar de file para docker

--- src/main/java/com/aprendiz/controller/AprendizController.java
+++ src/main/java/com/aprendiz/controller/AprendizController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aprendices")
+@RequestMapping("/api/v1/aprendiz")
+@CrossOrigin (origins = "http://localhost:5173")
 public class Aprendiz {
 
     @Autowired
